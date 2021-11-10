bd180e94de10789f380bc84b40a39bf5f876dc78
Chris "Mo" Mochinski <cmochinski@gmail.com>
2021-11-10 16:17:27 -0600

    testing some stuff and playing with buttons

--- App.js
+++ App.js
@@ -1,21 +1,88 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import {
+  StyleSheet,
+  Button,
+  View,
+  SafeAreaView,
+  Text,
+  Alert,
+  Image
+} from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+const Separator = () => <View style={styles.separator} />;
+
+const App = () => (
+  <SafeAreaView style={styles.container}>
+    <View>
+      <Text style={styles.title}>Welcome to this sweet lasagna button!</Text>
+      <Button
+        title="Lasagna"
+        onPress={() => Alert.alert("Simple Button pressed")}
+      />
     </View>
-  );
-}
+    <Separator />
+
+    {/* <Image
+        style={styles.tinyLogo}
+        source={require('@expo/snack-static/react-native-logo.png')}
+      /> */}
+
+    <Separator />
+
+    <View>
+      <Text style={styles.title}>
+        Here's another one...this one is pink. Weeeee!
+      </Text>
+      <Button
+        title="Press me"
+        color="red"
+        onPress={() => Alert.alert("Button with adjusted color pressed")}
+      />
+    </View>
+    <Separator />
+    <View>
+      <Text style={styles.title}>Disabled button.</Text>
+      <Button
+        title="Press me"
+        disabled
+        onPress={() => Alert.alert("Cannot press this one")}
+      />
+    </View>
+    <Separator />
+    <View>
+      <Text style={styles.title}>
+        Which button, left or right? One is right, one is wrong!
+      </Text>
+      <View style={styles.fixToText}>
+        <Button title="Left button" onPress={() => Alert.alert("Correct!!!")} />
+        <Button
+          title="Right button"
+          onPress={() => Alert.alert("Should have pressed left!")}
+        />
+      </View>
+    </View>
+  </SafeAreaView>
+);
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    marginHorizontal: 16,
+  },
+  title: {
+    textAlign: "center",
+    marginVertical: 8,
+  },
+  fixToText: {
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+  },
+  separator: {
+    marginVertical: 5,
+    borderBottomColor: "#737373",
+    borderBottomWidth: StyleSheet.hairlineWidth,
   },
 });
+
+export default App;
---
+++ README.md
@@ -0,0 +1,3 @@
+//important paste from Chris later
+
+** with mobile development, these apps get developed to mobil device
